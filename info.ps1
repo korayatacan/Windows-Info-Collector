b03aa71fa7125acb33f0880fa4939c8905878f39
Koray Atacan <korayatacan5@gmail.com>
2026-06-08 20:34:13 +0300

    Added RAM and Last Boot information

--- info.ps1
+++ info.ps1
@@ -1,17 +1,16 @@
 Write-Host "=== Windows Info Collector ==="
 Write-Host ""
 
-Write-Host "Computer Name:"
-hostname
-
+Write-Host "Date: $(Get-Date -Format 'dd MMMM yyyy dddd HH:mm:ss')"
+Write-Host "Computer Name: $(hostname)"
+Write-Host "Current User: $(whoami)"
 Write-Host ""
-Write-Host "Current User:"
-whoami
-
+Write-Host "Windows Version: $((Get-CimInstance Win32_OperatingSystem).Caption)"
+Write-Host "RAM: $([math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB,1)) GB"
 Write-Host ""
-Write-Host "Date:"
-Get-Date
-Write-Host ""
-Write-Host "Windows Version:"
 
-(Get-CimInstance Win32_OperatingSystem).Caption
+$lastBoot = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+$uptime = (Get-Date) - $lastBoot
+Write-Host "Last Boot: $($lastBoot.ToString("dd MMMM yyyy HH:mm:ss")) ($($uptime.Days) days ago)"
+Write-Host ""
+
