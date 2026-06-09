f97f29a5cf23d4e9f063bfcfe1a6024a531b7da5
Koray Atacan <korayatacan5@gmail.com>
2026-06-09 13:27:35 +0300

    improved output layout and added disk information

--- info.ps1
+++ info.ps1
@@ -5,12 +5,20 @@ Write-Host "Date: $(Get-Date -Format 'dd MMMM yyyy dddd HH:mm:ss')"
 Write-Host "Computer Name: $(hostname)"
 Write-Host "Current User: $(whoami)"
 Write-Host ""
-Write-Host "Windows Version: $((Get-CimInstance Win32_OperatingSystem).Caption)"
+
+
+write-Host "Hardware Information:"
+write-Host "----------------------"
 Write-Host "RAM: $([math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB,1)) GB"
+$disk = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'"
+$FreeSpace = $([math]::Round((Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace / 1GB, 1))
+$TotalSize = $([math]::Round((Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'").Size / 1GB, 1))
+Write-Host "Disk C: $($FreeSpace) GB Free / $($TotalSize) GB Total "
 Write-Host ""
 
+write-Host "Operating System Information:"
+write-Host "-----------------------------"
 $lastBoot = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
 $uptime = (Get-Date) - $lastBoot
 Write-Host "Last Boot: $($lastBoot.ToString("dd MMMM yyyy HH:mm:ss")) ($($uptime.Days) days ago)"
-Write-Host ""
-
+Write-Host "Windows Version: $((Get-CimInstance Win32_OperatingSystem).Caption)"
